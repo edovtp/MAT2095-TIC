78bcf47e2c41a985014f273b53e738ec0491d013
Eduardo Vásquez <evasquezt@uc.cl>
2022-09-16 20:48:41 -0300

    Directory changes

--- R/experiments/00-helpers.R
+++ R/experiments/00-helpers.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(collections)
 
 
@@ -60,7 +61,7 @@ rdp_data <- function(n, M, G0, G0_params) {
     all_values <- counter$keys()
     prev_n <- length(all_values)
     all_values[[prev_n + 1]] <- candidate
-    
+
     # Probabilities
     norm_term <- 1 / (M + i - 1)
     old_freq <- unlist(counter$values())
@@ -77,11 +78,11 @@ rdp_data <- function(n, M, G0, G0_params) {
   }
 
   dp_sample <- simplify2array(dp_sample)
-  
+
   if (length(new_value) != 1) {
     dp_sample <- t(dp_sample)
   }
-  
+
   return(dp_sample)
 }
 
@@ -90,21 +91,37 @@ rnormigamma <- function(n, mu, lambda, alpha, beta) {
   # We save the values
   samples_s2 <- vector(mode = "numeric", length = n)
   samples_mu <- vector(mode = "numeric", length = n)
-  
+
   # We take the samples
   for (i in 1:n) {
     inv_sigma2 <- rgamma(1, shape = alpha, rate = beta)
     sigma2_sampled <- 1 / inv_sigma2
     mu_sampled <- rnorm(1, mean = mu, sd = sqrt(sigma2_sampled / lambda))
-    
+
     samples_s2[i] <- sigma2_sampled
     samples_mu[i] <- mu_sampled
   }
-  
-  if (n == 1){
+
+  if (n == 1) {
     samples <- c(samples_mu, samples_s2)
   } else {
     samples <- cbind(samples_mu, samples_s2)
   }
   return(samples)
+}
+
+# Non-standardize t-distribution ----------------------------------------------------
+## Credits: Richard McElreath, rethinking package
+dstudent <- function(x, nu = 2, mu = 0, sigma = 1, log = FALSE) {
+  y <- lgamma((nu + 1) / 2) - lgamma(nu / 2) - 0.5 * log(pi * nu) - log(sigma) +
+    (-(nu + 1) / 2) * log(1 + (1 / nu) * ((x - mu) / sigma)^2)
+  if (log == FALSE) y <- exp(y)
+
+  return(y)
+}
+
+rstudent <- function(n, nu=2, mu=0, sigma=1) {
+  y <- rt(n, df=nu)
+  y <- y*sigma + mu
+  return(y)
 }
