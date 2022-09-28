8ae71c88aa34c89e5922498faf40e4e713028990
Eduardo Vásquez <evasquezt@uc.cl>
2022-09-28 14:34:02 -0300

    EW (1995) - Final algorithm implemented

--- R/01_EW_1995/algorithms.R
+++ R/01_EW_1995/algorithms.R
@@ -5,7 +5,7 @@ ew_algorithm1 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
   n <- length(y)
 
   # Array in which we'll save the samples (plus one for the initial values)
-  samples <- array(numeric(n * (iter + 1) * 2), dim = c(iter + 1, n, 2))
+  samples <- array(numeric((iter + 1) * n * 2), dim = c(iter + 1, n, 2))
 
   # Initial values
   for (i in 1:n) {
@@ -17,7 +17,7 @@ ew_algorithm1 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
       beta   = (S + (y[i] - m)^2 / (1 + tau)) / 2
     )
   }
-
+  
   # Start of the algorithm
   for (n_sample in 2:(iter + 1)) {
     prev_sample <- samples[(n_sample - 1), , ]
@@ -48,6 +48,8 @@ ew_algorithm1 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
       } else {
         samples[n_sample, i, ] <- prev_sample[idx_new, ]
       }
+      
+      prev_sample[i, ] <- samples[n_sample, i, ]
     }
   }
 
@@ -86,11 +88,9 @@ ew_algorithm2 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
   for (n_sample in 2:(iter + 1)) {
     prev_mt <- mt_samples[(n_sample - 1), ]
     prev_pi <- pi_samples[(n_sample - 1), , ]
-
-    # Update m and tau
     unique_pi <- unique(prev_pi)
 
-    ## m
+    ## Update m
     Vbar <- 1 / sum(1 / unique_pi[, 2])
     x <- A / (A + prev_mt[2] * Vbar)
     m <- rnorm(
@@ -99,13 +99,13 @@ ew_algorithm2 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
       sd   = sqrt(x * prev_mt[2] * Vbar)
     )
 
-    ## tau
+    ## Update tau
     k <- nrow(unique_pi)
     K <- sum((unique_pi[, 1] - m)^2 / unique_pi[, 2])
-    tau <- 1 / rgamma(1, (w + k) / 2, (W + K) / 2)
+    tau <- 1 / rgamma(1, (w + k) / 2, rate = (W + K) / 2)
 
     mt_samples[n_sample, ] <- c(m, tau)
-
+    
     # Update pi for each observation
     for (i in 1:n) {
       # Weights
@@ -132,6 +132,8 @@ ew_algorithm2 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
       } else {
         pi_samples[n_sample, i, ] <- prev_pi[idx_new, ]
       }
+      
+      prev_pi[i, ] <- pi_samples[n_sample, i, ]
     }
   }
 
@@ -141,6 +143,112 @@ ew_algorithm2 <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
     mt = mt_samples[(warmup + 2):(iter + 1), ]
   ))
 }
+
+# EW (1995) - Algorithm 3 -----------------------------------------------------------
+ew_algorithm <- function(y, prior_par, iter, warmup = floor(iter / 2)) {
+  # prior_par should contain: a, b, A, w, W, s and S
+  list2env(prior_par, envir = environment())
+  n <- length(y)
+  
+  # Arrays in which we'll save the samples (plus one for the initial values)
+  et_samples <- vector(mode = "numeric", length = iter)
+  al_samples <- vector(mode = "numeric", length = iter + 1)
+  mt_samples <- array(numeric((iter + 1) * 2), dim = c(iter + 1, 2))
+  pi_samples <- array(numeric((iter + 1) * n * 2), dim = c(iter + 1, n, 2))
+  
+  # Initial values
+  ## Choose a value of alpha from its prior
+  alpha <- rgamma(1, a, rate = b)
+  al_samples[1] <- alpha
+  
+  ## Choose values of m and tau from its priors
+  tau <- 1 / rgamma(1, w/2, rate = W/2)
+  m <- rnorm(1, 0, 1)
+  mt_samples[1, ] <- c(m, tau)
+  
+  ## Initial values of pi
+  for (i in 1:n) {
+    pi_samples[1, i, ] <- rnormigamma(
+      n      = 1,
+      mu     = (m + tau * y[i]) / (1 + tau),
+      lambda = (1 + tau) / tau,
+      alpha  = (1 + s) / 2,
+      beta   = (S + (y[i] - m)^2 / (1 + tau)) / 2
+    )
+  }
+  
+  # Start of the algorithm
+  for (n_sample in 2:(iter + 1)) {
+    prev_al <- al_samples[n_sample - 1]
+    prev_mt <- mt_samples[(n_sample - 1), ]
+    prev_pi <- pi_samples[(n_sample - 1), , ]
+    
+    unique_pi <- unique(prev_pi)
+    k         <- nrow(unique_pi)
+    
+    ## Update alpha
+    eta <- rbeta(1, prev_al + 1, n)
+    et_samples[n_sample] <- eta
+    odds_weight <- (a + k - 1)/(n*(b - log(eta)))
+    weight <- odds_weight/(1 + odds_weight)
+    component <- sample(c(0, 1), 1, prob = c(weight, 1 - weight))
+    alpha <- rgamma(1, a + k - 1 * component, rate = b - log(eta))
+    al_samples[n_sample] <- alpha
+    
+    ## Update m and tau
+    # m
+    Vbar <- 1 / sum(1 / unique_pi[, 2])
+    x <- A / (A + prev_mt[2] * Vbar)
+    m <- rnorm(
+      n    = 1,
+      mean = x * Vbar * sum(unique_pi[, 1] / unique_pi[, 2]),
+      sd   = sqrt(x * prev_mt[2] * Vbar)
+    )
+    
+    # tau
+    K <- sum((unique_pi[, 1] - m)^2 / unique_pi[, 2])
+    tau <- 1 / rgamma(1, (w + k)/2, rate = (W + K)/2)
+    
+    mt_samples[n_sample, ] <- c(m, tau)
+    
+    ## Update pi for each observation
+    for (i in 1:n) {
+      # Weights
+      q_weights <- vector(mode = "numeric", length = n)
+      q_weights[i] <- alpha * dstudent(y[i],
+                                       nu = s, mu = m,
+                                       sigma = sqrt((1 + tau) * S / s)
+      )
+      q_weights[-i] <- dnorm(y[i], prev_pi[-i, 1], sqrt(prev_pi[-i, 2]))
+      
+      # New sample
+      idx_new <- sample(n, size = 1, prob = q_weights)
+      if (idx_new == i) {
+        pi_samples[n_sample, i, ] <- rnormigamma(
+          n      = 1,
+          mu     = (m + tau * y[i]) / (1 + tau),
+          lambda = (1 + tau) / tau,
+          alpha  = (1 + s) / 2,
+          beta   = (S + (y[i] - m)^2 / (1 + tau)) / 2
+        )
+      } else {
+        pi_samples[n_sample, i, ] <- prev_pi[idx_new, ]
+      }
+      
+      # Update previous pi
+      prev_pi[i, ] <- pi_samples[n_sample, i, ]
+    }
+  }
+  
+  # Sum 2 because of the initial values
+  return(list(
+    eta = et_samples[(warmup + 2):(iter + 1)],
+    alpha = al_samples[(warmup + 2):(iter + 1)],
+    pi = pi_samples[(warmup + 2):(iter + 1), , ],
+    mt = mt_samples[(warmup + 2):(iter + 1), ]
+  ))
+}
+
 
 # ew_algorithm1 <- function(y, prior_par, iter, warmup = floor(iter/2)){
 #   # prior_par should contain: alpha (precision parameter), m, tau, s and S
