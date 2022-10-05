a36a32c9c2cc5939243cca6f21fcac300a58a9dd
Eduardo Vásquez <evasquezt@uc.cl>
2022-10-05 09:54:36 -0300

    EW-J Final algorithm implemented (2)

---
+++ Julia/code/03_ew.jl
@@ -0,0 +1,149 @@
+include("00_extras.jl")
+include("01_dirichlet_process.jl")
+include("02_dirichlet_process_mixtures.jl")
+
+
+function ew_algorithm(y, prior_par, iter, warmup=floor(Int64, iter / 2))
+    """
+    Implementation of the final algorithm given in Escobar & West (1995)
+
+    y         : data to fit the model
+    prior_par : prior parameters (a, b, A, w, W, s, S)
+    """
+    n = length(y)
+    a, b, A, w, W, s, S = prior_par
+    et_samples = Vector{Float64}(undef, iter)
+    al_samples = Vector{Float64}(undef, iter + 1)
+    mt_samples = Array{Float64,2}(undef, (iter + 1, 2))
+    pi_samples = Array{Float64,3}(undef, (iter + 1, n, 2))
+
+    # Initial values
+    ## alpha
+    alpha = rand(Gamma(a, 1 / b))
+    al_samples[1] = alpha
+
+    ## m and tau
+    tau = rand(InverseGamma(w / 2, W / 2))
+    m = rand(Normal(0, 1))
+    mt_samples[1, :] = [m, tau]
+
+    ## pi
+    for i in 1:n
+        xi = (m + tau * y[i]) / (1 + tau)
+        X = tau / (1 + tau)
+        Si = S + (y[i] - m)^2 / (1 + tau)
+
+        post = NormalInverseGamma(xi, X, (1 + s) / 2, Si / 2)
+        pi_samples[1, i, :] .= rand(post)
+    end
+
+    # Start of the algorithm
+    prev_pi = pi_samples[1, :, :]
+    for n_sample in 2:(iter+1)
+        unique_pi = unique(prev_pi, dims=1)
+        k = size(unique_pi)[1]
+
+        ## Update alpha
+        eta = rand(Beta(alpha + 1, n))
+        odds_weight = (a + k - 1) / (n * (b - log(eta)))
+        weight = odds_weight / (1 + odds_weight)
+        component = sample([0, 1], Weights([weight, 1 - weight]))
+        alpha = rand(Gamma(a + k - 1 * component, 1 / (b - log(eta))))
+
+        ## Update m and tau
+        Vbar = 1 / sum(1 ./ unique_pi[:, 2])
+        x = A / (A + tau * Vbar)
+        m = rand(
+            Normal(x * Vbar * sum(unique_pi[:, 1] ./ unique_pi[:, 2]), sqrt(x * tau * Vbar))
+        )
+        K = sum((unique_pi[:, 1] .- m) .^ 2 ./ unique_pi[:, 2])
+        tau = rand(InverseGamma((w + k) / 2, (W + K) / 2))
+
+        ## Update pi for each observation
+        xi = (m .+ tau .* y) ./ (1 + tau)
+        X = tau / (1 + tau)
+        Si = S .+ (y .- m) .^ 2 ./ (1 + tau)
+        M = (1 + tau) * S / s
+        for i in 1:n
+            q_weights = Vector{Float64}(undef, n)
+            q_weights[i] = alpha * pdf(TDist(s), (y[i] - m) / sqrt(M)) / sqrt(M)
+            q_weights[1:end.!=i] = map(
+                x -> pdf(Normal(x[1], sqrt(x[2])), y[i]),
+                eachrow(prev_pi[1:end.!=i, :])
+            )
+
+            idx_new = StatsBase.sample(1:n, Weights(q_weights))
+            if idx_new == i
+                prev_pi[i, :] .= rand(
+                    NormalInverseGamma(xi[i], X, (1 + s) / 2, Si[i] / 2)
+                )
+            else
+                prev_pi[i, :] .= prev_pi[idx_new, :]
+            end
+        end
+
+        # Save the values
+        et_samples[n_sample-1] = eta
+        al_samples[n_sample] = alpha
+        mt_samples[n_sample, :] .= [m, tau]
+        pi_samples[n_sample, :, :] .= prev_pi
+    end
+
+    return (et_samples[(warmup+2):end],
+        al_samples[(warmup+2):end],
+        mt_samples[(warmup+2):end, :],
+        pi_samples[(warmup+2):end, :, :])
+end
+
+
+function ew_algorithm1(y, prior_par, iter, warmup=floor(Int64, iter / 2))
+    """
+    Implementation of the first algorithm given in Escobar & West (1995)
+
+    y         : data to fit the model
+    prior_par : prior parameters (alpha, m, tau, s and S)
+    """
+    n = length(y)
+    alpha, m, tau, s, S = prior_par
+    samples = Array{Float64,3}(undef, (iter + 1, n, 2))
+
+    # Initial values
+    for i in 1:n
+        xi = (m + tau * y[i]) / (1 + tau)
+        X = tau / (1 + tau)
+        Si = S + (y[i] - m)^2 / (1 + tau)
+
+        post = NormalInverseGamma(xi, X, (1 + s) / 2, Si / 2)
+        samples[1, i, :] .= rand(post)
+    end
+
+    # Start of the algorithm
+    prev_sample = samples[1, :, :]
+    for n_sample in 2:(iter+1)
+        # Update of each component
+        for i in 1:n
+            xi = (m + tau * y[i]) / (1 + tau)
+            X = tau / (1 + tau)
+            Si = S + (y[i] - m)^2 / (1 + tau)
+            M = (1 + tau) * S / s
+            q_weights = Vector{Float64}(undef, n)
+
+            q_weights[i] = alpha * pdf(TDist(s), (y[i] - m) / sqrt(M)) / sqrt(M)
+            q_weights[1:end.!=i] = map(
+                x -> pdf(Normal(x[1], sqrt(x[2])), y[i]),
+                eachrow(prev_sample[1:end.!=i, :])
+            )
+
+            idx_new = StatsBase.sample(1:n, Weights(q_weights))
+            if idx_new == i
+                prev_sample[i, :] .= rand(NormalInverseGamma(xi, X, (1 + s) / 2, Si / 2))
+            else
+                prev_sample[i, :] .= prev_sample[idx_new, :]
+            end
+        end
+
+        samples[n_sample, :, :] .= prev_sample
+    end
+
+    return samples[(warmup+2):(iter+1), :, :]
+end
---
+++ Julia/code/03_examples.jl
@@ -0,0 +1,122 @@
+using Plots
+using StatsPlots
+
+include("03_ew.jl")
+
+
+### Recovering parameters
+Random.seed!(219)
+n, alpha, m, tau, s, S = 50, 1, 0, 100, 50, 2
+a1_data = tic_rdpm_normal(n, alpha, m, tau, s, S)
+
+histogram(a1_data.y, bins=30, label="Simulated data")
+vline!(unique(a1_data.params[:, 1]), label="Components")
+
+prior_par = (alpha, m, tau, s, S)
+@time test_a1_rp = ew_algorithm1(a1_data.y, prior_par, 10000);
+
+sample_means = mapslices(mean, test_a1_rp, dims=[1])
+
+scatter(a1_data.params[:, 1], sample_means[1, :, 1], ms=3, ma=0.3, label="")
+Plots.abline!(1, 0, label="")
+
+scatter(a1_data.params[:, 2], sample_means[1, :, 2], ms=3, ma=0.3, label="")
+Plots.abline!(1, 0, label="")
+
+k_sim = map(
+    x -> size(unique(x, dims=1))[1],
+    eachslice(test_a1_rp, dims=1)
+)
+k_mean = mean(k_sim)
+
+bar(sort(unique(k_sim)), counts(k_sim), label="Sampled values of k")
+vline!([a1_data.k], label="Real value")
+
+### Recovering density
+Random.seed!(219)
+mmodel = MixtureModel(
+    Normal[Normal(-5, 1), Normal(-1, 1), Normal(0, 1), Normal(5, 1)],
+    [0.15, 0.25, 0.3, 0.3]
+)
+
+n = 100
+probs = [0.15, 0.25, 0.3, 0.3]
+nmeans = [-5, -1, 0, 5]
+components = sample(1:4, Weights(probs), n)
+y = rand(Normal(0, 1), n) + nmeans[components]
+
+# See https://github.com/JuliaPlots/StatsPlots.jl/issues/458
+histogram(y, normalize=true, bins=30, alpha=0.3, label="samples")
+plot!(mmodel, components=false, label="Real density")
+vline!([-5, -1, 0, 5], c="red")
+
+alpha, m, tau, s, S = 1, 0, 100, 4, 2
+prior_par = (alpha, m, tau, s, S)
+test_a1_rd = ew_algorithm1(y, prior_par, 10000)
+sample_means = mapslices(mean, test_a1_rd, dims=[1])
+
+scatter(nmeans[components], sample_means[1, :, 1], ms=3, ma=0.3, label="")
+Plots.abline!(1, 0, label="")
+
+function cond_dens(y)
+    M = (1 + tau) * S / s
+    s1 = alpha * pdf(TDist(s), (y - m) / sqrt(M)) / sqrt(M)
+    s2 = [sum(map(x -> pdf(Normal(x[1], sqrt(x[2])), y), eachrow(sample)))
+          for sample in eachslice(test_a1_rd, dims=1)]
+    dens = (s1 .+ s2) ./ (alpha + n)
+
+    return mean(dens)
+end
+
+@time dens_est = [cond_dens(y) for y in range(-8, 8, length=500)]
+
+plot(mmodel, components=false, label="Real density")
+plot!(range(-8, 8, length=500), dens_est, label="Estimated density")
+
+
+## Galaxies example
+velocities = [9172, 9558, 10406, 18419, 18927, 19330, 19440, 19541,
+    19846, 19914, 19989, 20179, 20221, 20795, 20875, 21492,
+    21921, 22209, 22314, 22746, 22914, 23263, 23542, 23711,
+    24289, 24990, 26995, 34279, 9350, 9775, 16084, 18552,
+    19052, 19343, 19473, 19547, 19856, 19918, 20166, 20196,
+    20415, 20821, 20986, 21701, 21960, 22242, 22374, 22747,
+    23206, 23484, 23666, 24129, 24366, 25633, 32065, 9483,
+    10227, 16170, 18600, 19070, 19349, 19529, 19663, 19863,
+    19973, 20175, 20215, 20629, 20846, 21137, 21814, 22185,
+    22249, 22495, 22888, 23241, 23538, 23706, 24285, 24717,
+    26960, 32789] ./ 1000
+
+histogram(velocities, bins=1:40, label="")
+
+a, b, A, w, W, s, S = 2, 4, 1000, 1, 100, 4, 2
+prior_par = (a, b, A, w, W, s, S)
+
+Random.seed!(219)
+N = 10000
+warmup = 2000
+@time g_eta, g_alpha, g_mt, g_pi = ew_algorithm(velocities, prior_par, N, warmup);
+
+## Posterior predictive density p(y|D)
+n = length(velocities)
+function cond_dens(y)
+    s1 = [
+        g_alpha[i] * pdf(TDist(s), (y - g_mt[i, 1]) / sqrt(1 + g_mt[i, 2] * S / s)) /
+        sqrt(1 + g_mt[i, 2] * S / s) for i in 1:(N-warmup)
+    ]
+
+    s2 = [
+        sum(map(x -> pdf(Normal(x[1], sqrt(x[2])), y), eachrow(sample)))
+        for sample in eachslice(g_pi, dims=1)
+    ]
+
+    dens = (s1 .+ s2) ./ (g_alpha .+ n)
+
+    return mean(dens)
+end
+
+y_grid = range(8, 40, length=500)
+@time dens_est = [cond_dens(y) for y in y_grid];
+
+histogram(velocities, bins=1:40, label="", normalize=true)
+plot!(y_grid, dens_est, label="Estimated density", linewidth=3)
