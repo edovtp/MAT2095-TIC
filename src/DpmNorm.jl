b2ae7a15b5ea966862b6cdb63c16c5f7f6ed23fc
edovtp <evasquezt@uc.cl>
2023-03-06 19:27:35 -0300

    A2 - DPM of Normals with conjugate prior

--- src/DpmNorm.jl
+++ src/DpmNorm.jl
@@ -77,8 +77,159 @@ function DpmNorm1f(y, prior_par, iter, warmup=floor(Int64, iter / 2))
     return (μ_samples=μ_samples, V_samples=V_samples, θ_new=θ_new)
 end
 
-function DpmNorm2()
+function _relabel(c, θ_unique)
+    c_re = Vector{Int64}(undef, length(c))
 
+    c_unique = unique(c)
+    θ_re = θ_unique[c_unique]
+    for i in eachindex(c_unique)
+        c_re[c.==c_unique[i]] .= i
+    end
+
+    return c_re, θ_re
+end
+
+function DpmNorm2(y, prior_par, iter, init_c="same", warmup=floor(Int64, iter / 2))
+    """
+    Implementation of Algorithm 2-3 (Neal, 2000) for the Normal model with conjugate prior G0 and
+    random hyperparameters. That is,
+
+        y_i | θ_i ∼ Normal(μ_i, V_i)
+        θ_i | G ∼ G
+        G ∼ DP(M, G_η)
+        G_η = NIG(m, γ, s/2, S/2)
+        m ∼ Normal(a, A)
+        γ ∼ IG(w/2, W/2)
+        M ∼ Gamma(α, β)
+
+    y         : data to fit the model
+    prior_par : prior parameters (a, A, w, W, α, β, s, S)
+    iter      : number of iterations including warmup
+    init_c    : initial value for c, with "same" they all start in the same cluster, "diff" to put
+                them all in different clusters
+    """
+    n = length(y)
+    a, A, w, W, α, β, s, S = prior_par
+    total_samples = iter - warmup
+    samples = Dict(
+        :μ => Array{Float64}(undef, total_samples, n),
+        :V => Array{Float64}(undef, total_samples, n),
+        :c => Array{Int64}(undef, total_samples, n),
+        :m => Vector{Float64}(undef, total_samples),
+        :γ => Vector{Float64}(undef, total_samples),
+        :M => Vector{Float64}(undef, total_samples),
+        :ϕ => Vector{Float64}(undef, total_samples),
+        :π => Vector{Float64}(undef, total_samples),
+        :θ_new => Vector{Tuple}(undef, total_samples)
+    )
+
+    # Initial values
+    M = rand(Gamma(α, 1 / β))
+    γ = rand(InverseGamma(w / 2, W / 2))
+    m = rand(Normal(a, A))
+    G_η = NormalInverseGamma(m, γ, s / 2, S / 2)
+    if init_c == "same"
+        c = ones(Int64, n)
+        θ_unique = [rand(G_η)]
+    elseif init_c == "diff"
+        c = Vector{Int64}(1:n)
+        θ_unique = [rand(G_η) for _ in 1:n]
+    end
+
+    # Start of the algorithm
+    for n_sample in 1:iter
+        # Update M
+        k = length(θ_unique)
+        ϕ = rand(Beta(M + 1, n))
+        odds_weight = (α + k - 1) / (n * (β - log(ϕ)))
+        weight = odds_weight / (1 + odds_weight)
+        component = sample([0, 1], Weights([weight, 1 - weight]))
+        M = rand(Gamma(α + k - 1 * component, 1 / (β - log(ϕ))))
+
+        # Update m, γ
+        μ_unique = [θ[1] for θ in θ_unique]
+        V_unique = [θ[2] for θ in θ_unique]
+        V_bar = 1 / sum(1 ./ V_unique)
+        x = A / (A + γ * V_bar)
+        m = rand(Normal((1 - x) * a + x * V_bar * sum(μ_unique ./ V_unique), sqrt(x * γ * V_bar)))
+        K = sum((μ_unique .- m) .^ 2 ./ V_unique)
+        γ = rand(InverseGamma((w + k) / 2, (W + K) / 2))
+
+        # Update c
+        for i in 1:n
+            current_c = c[i]
+            c_ = c[1:end.!=i] # c minus i
+            y_ = y[1:end.!=i]
+
+            c_unique_ = unique(c_)
+            k_ = length(c_unique_)
+            weights = Vector{Float64}(undef, k_ + 1)
+            for (idx, cluster) in enumerate(c_unique_)
+                y_clust_ = y_[c_.==cluster]
+                n_j_ = length(y_clust_)
+
+                y_bar_ = mean(y_clust_)
+                m_j_ = (m + γ * sum(y_clust_)) / (1 + γ * n_j_)
+                γ_j_ = γ / (1 + γ)
+                s_j_ = s + n_j_
+                S_j_ = S + n_j_ / (1 + n_j_ * γ) * (y_bar_ - m)^2 + sum((y_clust_ .- y_bar_) .^ 2)
+
+                scale_t = sqrt(S_j_ / s_j_ * (1 + γ_j_))
+                weights[idx] = pdf(TDist(s_j_), (y[i] - m_j_) / scale_t) / scale_t
+            end
+            # New cluster
+            scale_t = sqrt(S / s * (1 + γ))
+            weights[k_ + 1] = pdf(TDist(s), (y[i] - m) / scale_t) / scale_t
+            idx_cluster_new = StatsBase.sample(1:(k_ + 1), Weights(weights))
+            if idx_cluster_new == (k_ + 1)
+                G_η = NormalInverseGamma(m, γ, s / 2, S / 2)
+                if length(θ_unique) == (k_ + 1)
+                    θ_unique[k_ + 1] = rand(G_η)
+                else
+                    c[i] = maximum(c_unique_) + 1
+                    push!(θ_unique, rand(G_η))
+                end
+            else
+                c[i] = c_unique_[idx_cluster_new]
+            end
+
+            c, θ_unique = _relabel(c, θ_unique)
+        end
+
+        # Update θ_unique
+        n_j_list = Vector{Int64}(undef, length(θ_unique))
+        for j in eachindex(θ_unique)
+            y_clust = y[c .== j]
+            n_j = length(y_clust)
+            n_j_list[j] = n_j
+            y_bar = mean(y_clust)
+
+            m_j = (m + γ*sum(y_clust))/(1 + γ * n_j)
+            γ_j = γ / (1 + γ)
+            s_j = s + n_j
+            S_j = S + n_j / (1 + n_j * γ) * (y_bar - m)^2 + sum((y_clust .- y_bar) .^ 2)
+
+            θ_unique[j] = rand(NormalInverseGamma(m_j, γ_j, s_j/2, S_j/2))
+        end
+
+        if n_sample > warmup
+            samples[:ϕ][n_sample-warmup] = ϕ
+            samples[:π][n_sample-warmup] = weight
+            samples[:M][n_sample-warmup] = M
+            samples[:m][n_sample-warmup] = m
+            samples[:γ][n_sample-warmup] = γ
+            samples[:c][n_sample-warmup, :] = c
+            samples[:μ][n_sample-warmup, :] = [θ[1] for θ in θ_unique[c]]
+            samples[:V][n_sample-warmup, :] = [θ[2] for θ in θ_unique[c]]
+
+            # New θ
+            all_values = [θ_unique; rand(NormalInverseGamma(m, γ, s/2, S/2))]
+            wprobs = pweights([n_j_list; M])
+            samples[:θ_new][n_sample-warmup] = StatsBase.sample(all_values, wprobs)
+        end
+    end
+
+    return samples
 end
 
 function DpmNorm8()
--- src/examples/04_DpmNorm.jl
+++ src/examples/04_DpmNorm.jl
@@ -79,9 +79,9 @@ k_sim_mean = mean(k_sim);
 
 begin
     fig = Figure(resolution=(1280, 720))
-    
-    ax = Axis(fig[1, 1]; xticks = 3:11, xgridvisible=false, ygridvisible=false)
-    hist = barplot!(names(freq_k)[1], freq_k.array./2000; bins=30,
+
+    ax = Axis(fig[1, 1]; xticks=3:11, xgridvisible=false, ygridvisible=false)
+    hist = barplot!(names(freq_k)[1], freq_k.array ./ 2000; bins=30,
         color=ifelse.(3:11 .== 4, :red, :blue))
     k_sim = vlines!(k_sim_mean; color=:black, linewidth=10)
     ylims!(0, 0.27)
@@ -106,6 +106,66 @@ end
 CairoMakie.save("monography/figures/DPM - sticky clusters.png", fig)
 #endregion
 
-# A1 - Random hyperparameters, galaxies example
+# A2 - Random hyperparameters, galaxies example
 #region
+velocities = [9172, 9558, 10406, 18419, 18927, 19330, 19440, 19541,
+    19846, 19914, 19989, 20179, 20221, 20795, 20875, 21492,
+    21921, 22209, 22314, 22746, 22914, 23263, 23542, 23711,
+    24289, 24990, 26995, 34279, 9350, 9775, 16084, 18552,
+    19052, 19343, 19473, 19547, 19856, 19918, 20166, 20196,
+    20415, 20821, 20986, 21701, 21960, 22242, 22374, 22747,
+    23206, 23484, 23666, 24129, 24366, 25633, 32065, 9483,
+    10227, 16170, 18600, 19070, 19349, 19529, 19663, 19863,
+    19973, 20175, 20215, 20629, 20846, 21137, 21814, 22185,
+    22249, 22495, 22888, 23241, 23538, 23706, 24285, 24717,
+    26960, 32789] ./ 1000;
+
+
+prior_par_random = (0, 1000, 1, 100, 2, 4, 4, 2); # a, A, w, W, α, β, s, S
+Random.seed!(219);
+N = 10000;
+warmup = 8000;
+n = length(velocities);
+@time a2_samples = DpmNorm2(velocities, prior_par_random, N, "same", warmup);
+
+# Density estimation
+function cond_dens(y)
+    mean([pdf(Normal(θ[1], sqrt(θ[2])), y) for θ in a2_samples[:θ_new]])
+end;
+dpm_est = [cond_dens(y) for y in range(2, 40, length=500)];
+
+begin
+    CairoMakie.activate!()
+    fig = Figure()
+    xrange = range(2, 40; length=500)
+
+    ax = Axis(fig[1, 1]; xgridvisible=false, ygridvisible=false)
+    hist!(velocities; normalization=:pdf, bins=1:40)
+    d_dpm = lines!(xrange, dpm_est; color=:red, linewidth=3)
+
+    fig
+end
+
+# Inference about M
+k = map(x -> length(unique(x)), eachrow(a2_samples[:μ]));
+ϕ_π_k = zip(a2_samples[:ϕ], a2_samples[:π], k);
+
+function cond_dens_M(alpha)
+    aux = [p[2] * pdf(Gamma(2 + p[3], 1 / (4 - log(p[1]))), alpha) +
+           (1 - p[2]) * pdf(Gamma(2 + p[3] - 1, 1 / (4 - log(p[1]))), alpha) for p in ϕ_π_k]
+    mean(aux)
+end
+
+dens_M = [cond_dens_M(M) for M in range(0, 3; length=500)];
+begin
+    CairoMakie.activate!()
+    fig = Figure()
+    xrange = range(0, 3; length=500)
+
+    ax = Axis(fig[1, 1]; xgridvisible=false, ygridvisible=false)
+    d_prior = lines!(xrange, x -> pdf(Gamma(2, 1 / 4), x))
+    d_post = lines!(xrange, dens_M; color=:red)
+    fig
+end
+
 #endregion
